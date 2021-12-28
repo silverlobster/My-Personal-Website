3102137bf94fe0af0b8b980fee2b4468c97dfb2e
Xing Wu <41498567+silverlobster@users.noreply.github.com>
2021-12-28 16:32:07 -0500

    added picture and description

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ var app = express();
 app.use(express.static(__dirname + '/public')); //__dir and not _dir
 var port = 8000; // you can use any port
 app.listen(process.env.PORT || port);
-console.log('server on' + port);
+console.log('http://localhost:' + port);
